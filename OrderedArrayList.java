fedfbecd76aa37f947dcd14cf24e76a0dd6d8bba
Elliot <ebossi10@stuy.edu>
2020-12-10 19:19:55 -0500

    finished ordered arraylist

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,3 +1,5 @@
+
+
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
   public OrderedArrayList(){
     super();
@@ -7,7 +9,23 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(i);
   }
 
+  public boolean add(T e){
+    int x = 0;
+    while ((x < super.size()) && (e.compareTo(super.get(x)) > 0)){
+      x = x + 1;
+    }
+    super.add(x, e);
+    return true;
+  }
 
+  public void add(int i, T e){
+    add(e);
+  }
 
+  public T set(int i, T e){
+    super.remove(i);
+    add(e);
+    return e;
+  }
 
 }
